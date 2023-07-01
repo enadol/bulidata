6de9f89ad60a466db414d4ad5e21892465b145b6
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-01 14:56:08 +0200

    Update BuliData.py

--- BuliData.py
+++ BuliData.py
@@ -18,7 +18,7 @@ df = pd.read_csv("results2022-2023201_overall.csv")
 df.Attendance = df.Attendance.str.replace(',', '').astype(int)
 
 with col1:
-    col1.header("Visualizations")
+    #col1.header("Visualizations")
     plot_options = ["Posiciones", "Puntos", "Público", "Goles", "Diff. Goles", "Partidos", "xG", "Goleadores"]
     selected_plot = col1.selectbox("Seleccione un gráfico", plot_options)
 
