dac19b5305ff636dbe10b7ce0ac5e3172be31c34
Enrique Lopez Magallon <enadol@yahoo.com>
2023-07-02 00:18:24 +0200

    Actualizar BuliData.py

--- BuliData.py
+++ BuliData.py
@@ -9,6 +9,7 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(layout="wide")
 #with st.container():
 col1, col2 = st.columns(2)
 #col1, col2=st.columns([1,3])
